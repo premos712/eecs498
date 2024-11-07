070137f622a8c97d5922ec13e54125ba1289d3f4
Fangyi Dai <fdai@sgx.eecs.umich.edu>
2024-11-07 12:52:17 -0500

    Initial commit of Hello World SGX project

---
+++ build/enclave/CMakeFiles/enclave.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "helloworld_t.h"
+  "helloworld_t.c"
+  "helloworld_args.h"
+  "CMakeFiles/enclave.dir/enc.c.o"
+  "CMakeFiles/enclave.dir/helloworld_t.c.o"
+  "enclave.pdb"
+  "enclave"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/enclave.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/host/CMakeFiles/helloworld_host.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "helloworld_u.h"
+  "helloworld_u.c"
+  "helloworld_args.h"
+  "CMakeFiles/helloworld_host.dir/host.c.o"
+  "CMakeFiles/helloworld_host.dir/helloworld_u.c.o"
+  "helloworld_host.pdb"
+  "helloworld_host"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/helloworld_host.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
